b5d29870df10af9be89504651bda2ff30c019e9a
Nikita <nikita.garanin27@gmail.com>
2026-01-29 21:00:35 -0500

    swtched to nasm

--- code/build.sh
+++ code/build.sh
@@ -1,1 +1,3 @@
-jwasm -bin rom.asm
+nasm -f bin main.asm -o main.bin
+nasm -f bin reset.asm -o reset.bin
+cmd /c "copy /b main.bin + reset.bin bios.bin"
---
+++ code/main.asm
@@ -0,0 +1,15 @@
+; main.asm
+BITS 16
+ORG 0x8000
+
+start:
+    cli             ; Disable interrupts
+    mov ax, 0xF000  ; Setup segments
+    mov ds, ax
+    mov ss, ax
+    mov sp, 0xFFFE  ; Stack at top of segment
+    
+    ; --- Your code starts here ---
+    mov al, '!'
+    hlt
+    times 32752 - ($ - $$) db 0x90 ; Pad with NOPs up to the vector space
