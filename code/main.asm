004074ee4e5846960a81292d616cb1ad0f85b6bb
Nikita <nikita.garanin27@gmail.com>
2026-02-15 18:35:16 -0500

    e

--- code/main.asm
+++ code/main.asm
@@ -5,17 +5,17 @@ ORG 0x8000
 ;uart starts at i/o 0
 
 start:
-    cli             ; Disable interrupts
-    mov ax, 0xF000  ; Setup segments
-    mov ds, ax
-    mov ss, ax
-    mov sp, 0xFFFE  ; Stack at top of segment
 
     mov al, 11111111b ; set port B to output
     out 2, al
     
     mov al, 0x55
     out 0, al
+
+    mov ax, 0xF000  ; Setup segments
+    mov ds, ax
+    mov ss, ax
+    mov sp, 0xFFFE  ; Stack at top of segment
 
 
     loop1:
