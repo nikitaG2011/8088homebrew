9aca210515717c2432e97a6cdffb6a92baf6cf71
Nikita <nikita.garanin27@gmail.com>
2026-02-21 20:31:26 -0500

    FIXY UPIE

--- code/main.asm
+++ code/main.asm
@@ -5,22 +5,34 @@ ORG 0x8000
 ;uart starts at i/o 0
 
 start:
-
-    mov al, 11111111b ; set port B to output
-    out 2, al
-    
-    mov al, 0x55
-    out 0, al
-
     mov ax, 0xF000  ; Setup segments
     mov ds, ax
     mov ss, ax
     mov sp, 0xFFFE  ; Stack at top of segment
 
+code:
+    mov al, 0x55 
+    out 0, al
+    mov ax, 1 
+    out 0, ax
 
-    loop1:
-
+loop1:
+    out 0, ax
+    call delay
+    add ax, 1
     jmp loop1
+
+delay:
+    push ax
+    mov ax, 0xFF
+delay_loop:
+    sub ax, 1
+    cmp ax, 0
+    jne delay_loop
+    ret
+
+
+
 
     ; end of code space
     times 32752 - ($ - $$) db 0x90
