fe85291a35f7ead8b1e39a9e0b173160b8428b72
Nikita <nikita.garanin27@gmail.com>
2026-03-26 20:04:36 -0400

    e

--- code/main.asm
+++ code/main.asm
@@ -2,15 +2,6 @@
 BITS 16
 ORG 0x8000
 
-LCD_CMD     EQU 0x00
-LCD_DATA    EQU 0x01
-
-
-
-
-LCD_CMD     EQU 0x00
-LCD_DATA    EQU 0x01
-
 init:
         
         mov ax, 0x7000
@@ -20,53 +11,10 @@ init:
         push cs
         pop es
 start:
-; push the length of the string on stack ("len")
-        mov ax, hello_str_end - hello_str
-        push ax
-; push the string address of the string on stack ("str")
-        mov ax, hello_str
-        push ax
-
-; call find_space() in the C-style
-        call find_space
-; tidy up the parameters (2 * 2 bytes)
-        add sp, 4
-
-        jmp $
-
-
-;   C declaration:
-;   int16_t find_space(const char* str, uint16_t len);
-find_space:
-        push bp
-        mov bp, sp
-
-        sub sp, 2
-
-        push di
-        mov di, [bp + 4]    ; load "str" to DI
-        mov cx, [bp + 6]    ; load "len" to CX
-        mov [bp - 2], di    ; save "str" to a local variable
-
-        mov al, ' '
-        repne scasb
-        je .1               ; jump over if found
-        mov ax, -1
-        jmp .ret
-.1:
-        mov ax, di
-        sub ax, [bp - 2]
-        dec ax
-.ret:
-        pop di
-
-        mov sp, bp
-        pop bp
-        ret
-
-hello_str:
-        DB "Hello World!"
-hello_str_end:
+        mov ax, 0x55
+        out 00010000b, ax
+loop:
+        jmp loop
 
 
 
