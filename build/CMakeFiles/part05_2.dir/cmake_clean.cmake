3dbf7374e6be743807048705d8f7bf95918448c3
Nikita <nikita.garanin27@gmail.com>
2026-05-24 22:02:22 -0400

    hmm

---
+++ build/CMakeFiles/part05_2.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/part05_2.dir/src/init.asm.obj"
+  "CMakeFiles/part05_2.dir/src/init.asm.obj.d"
+  "CMakeFiles/part05_2.dir/src/main.c.obj"
+  "CMakeFiles/part05_2.dir/src/main.c.obj.d"
+  "CMakeFiles/part05_2.dir/src/reset.asm.obj"
+  "CMakeFiles/part05_2.dir/src/reset.asm.obj.d"
+  "CMakeFiles/part05_2.dir/src/text_lcd.asm.obj"
+  "CMakeFiles/part05_2.dir/src/text_lcd.asm.obj.d"
+  "CMakeFiles/part05_2.dir/src/utils.asm.obj"
+  "CMakeFiles/part05_2.dir/src/utils.asm.obj.d"
+  "part05_2"
+  "part05_2.manifest"
+  "part05_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM_NASM C)
+  include(CMakeFiles/part05_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
