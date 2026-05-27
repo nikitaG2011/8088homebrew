17d0802b423e55ca4f773008b44dd2796eb9d05f
Nikita <nikita.garanin27@gmail.com>
2026-05-26 20:15:32 -0400

    Update atmega_TVout.ino

--- atmega_TVout/atmega_TVout.ino
+++ atmega_TVout/atmega_TVout.ino
@@ -3,33 +3,65 @@
 
 TVout TV;
 
-volatile bool ISR = false;
+volatile byte rxChar = 0;
+volatile bool charReady = false;
 
 void setup() {
 
-  pinMode(2, INPUT_PULLUP);
+  // 8-bit data bus
 
-  attachInterrupt( digitalPinToInterrupt(2), triggerISR, RISING );
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+
+  pinMode(8, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+
+  // STROBE interrupt pin
+  pinMode(2, INPUT);
+
+  attachInterrupt(
+    digitalPinToInterrupt(2),
+    dataInterrupt,
+    RISING
+  );
 
   TV.begin(_NTSC, 184, 72);
   TV.select_font(font6x8);
-
-  TV.println("Text demo");
-  TV.println("video output V0.1");
+  TV.println("video output V1.0");
 }
 
 void loop() {
 
-  if (ISR) {
+  if (charReady) {
 
     noInterrupts();
-    ISR = false;
+
+    byte c = rxChar;
+    charReady = false;
+
     interrupts();
 
-    TV.print("isr");
+    TV.print((char)c);
   }
 }
 
-void triggerISR() {
-  ISR = true;
+void dataInterrupt() {
+
+  rxChar = 0;
+
+  rxChar |= digitalRead(3)  << 0;
+  rxChar |= digitalRead(4)  << 1;
+  rxChar |= digitalRead(5)  << 2;
+  rxChar |= digitalRead(6)  << 3;
+
+  rxChar |= digitalRead(8)  << 4;
+  rxChar |= digitalRead(10) << 5;
+  rxChar |= digitalRead(11) << 6;
+  rxChar |= digitalRead(12) << 7;
+
+  charReady = true;
 }
