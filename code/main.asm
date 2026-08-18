9eefcef98d71114ef439127790cb3aa5ddc0e95b
Nikita <nikita.garanin27@gmail.com>
2026-08-18 17:18:36 -0400

    e

--- code/main.asm
+++ code/main.asm
@@ -22,22 +22,91 @@ init:
         sti
     
 code:
-        call lcd_delay
+        mov cx, 0x1200
+.1:     dec cx
+        jnz .1
+
+        mov al, 0x30
+        out LCD_CMD, al
+
+        mov cx, 0x0200
+.2:     dec cx
+        jnz .2
+
+        mov al, 0x30
+        out LCD_CMD, al
+
+        mov cx, 0x0040
+.3:     dec cx
+        jnz .3
+
+        mov al, 0x38    ; function set
+        out LCD_CMD, al
+
+        mov cx, 0x0020
+.4:     dec cx
+        jnz .4
+
+        mov al, 0x08    ; display off
+        out LCD_CMD, al
+
+        mov cx, 0x0020
+.5:     dec cx
+        jnz .5
+
+        mov al, 0x01    ; clear display
+        out LCD_CMD, al
+
+        mov cx, 0x0400
+.6:     dec cx
+        jnz .6
+
+        mov al, 0x02    ; return home
+        out LCD_CMD, al
+
+        mov cx, 0x0400
+.7:     dec cx
+        jnz .7
+
+        mov al, 0x06    ; entry mode set
+        out LCD_CMD, al
+
+        mov cx, 0x0020
+.8:     dec cx
+        jnz .8
+
+        mov al, 0x0c    ; display on, no cursor
+        out LCD_CMD, al
+
+        mov cx, 0x0020
+.9:     dec cx
+        jnz .9
+
         mov al, 'T'
         out LCD_DATA, al
 
-        call lcd_delay
+        mov cx, 0x0020
+.10:    dec cx
+        jnz .10
+
         mov al, 'E'
         out LCD_DATA, al
 
-        call lcd_delay
+        mov cx, 0x0020
+.11:    dec cx
+        jnz .11
+
         mov al, 'S'
         out LCD_DATA, al
 
-        call lcd_delay
+        mov cx, 0x0020
+.12:    dec cx
+        jnz .12
+
         mov al, 'T'
         out LCD_DATA, al
 
+        jmp $
 hang:
         jmp hang
         
@@ -52,8 +121,6 @@ INT_handler:
 
 ;include things here
 %include "functions.asm"
-
-
 
 
     ; end of code space
